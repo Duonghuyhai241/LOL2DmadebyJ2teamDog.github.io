4b2c20590e61e23dbbc793ff532fb337440a9eb5
Duonghuyhai241 <66892336+Duonghuyhai241@users.noreply.github.com>
2020-06-15 22:08:21 +0700

    Update loading.js

--- LOL2D-ver1/js/scenes/loading.js
+++ LOL2D-ver1/js/scenes/loading.js
@@ -12,9 +12,9 @@ function Loading() {
         if (!checkImages()) {
             // chieu thuc
             images.rocket = loadImage('images/jinx.png');
-            images.locxoay = loadImage('images/ys.png');
-            images.troiAnhSang = loadImage('images/lux.png');
-            images.banTay = loadImage('images/blit.png');
+            images.locxoay = loadImage('images/ys.jpg');
+            images.troiAnhSang = loadImage('images/lux.jpg');
+            images.banTay = loadImage('images/blit.jpg');
 
             // nhan vat
             images.yasuo = loadImage('images/character/yasuo.png');
